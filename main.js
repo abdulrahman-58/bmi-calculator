6615d92bdeb0f81ba4216a0448ec4bd361fbaa5a
abdulrahman <abdulrahmanazam58@gmail.com>
2024-04-22 03:46:19 -0700

    first commit

---
+++ main.js
@@ -0,0 +1,15 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+console.log(`\t "Welcome to BMI Calculator"`);
+let bmi = await inquirer.prompt([{
+        name: "weight",
+        type: "number",
+        message: "Weight(kg): "
+    },
+    {
+        name: "height",
+        type: "number",
+        message: "height(m): "
+    }
+]);
+console.log(`BMI :${bmi.weight / (bmi.height * bmi.height)}`);
